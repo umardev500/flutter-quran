ead674d1f1b27006f044c5c7ffa0c2c1843fbe30
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-09 21:59:04 +0700

    using scrollable positioned list

--- lib/components/organisms/quran_list.dart
+++ lib/components/organisms/quran_list.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:quran/components/molecules/quran_item.dart';
 import 'package:quran/models/quran_data.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class QuranList extends StatelessWidget {
   final List<QuranData> quranList;
-  const QuranList({super.key, required this.quranList});
+  final ItemScrollController itemScrollController;
+
+  const QuranList(
+      {super.key, required this.quranList, required this.itemScrollController});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ScrollablePositionedList.builder(
+      itemScrollController: itemScrollController,
       itemCount: quranList.length,
       itemBuilder: (context, index) {
         final item = quranList[index];
--- lib/screens/read_screen.dart
+++ lib/screens/read_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quran/components/organisms/quran_list.dart';
 import 'package:quran/models/quran_data.dart';
 import 'package:quran/repository/quran_repository.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class ReadScreen extends StatefulWidget {
   final int sura;
@@ -14,6 +15,12 @@ class ReadScreen extends StatefulWidget {
 
 class _ReadScreenState extends State<ReadScreen> {
   List<QuranData> quranList = [];
+  final ItemScrollController _itemScrollController = ItemScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   void initState() {
@@ -40,9 +47,17 @@ class _ReadScreenState extends State<ReadScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _itemScrollController.scrollTo(
+              index: 100, duration: const Duration(milliseconds: 300));
+        },
+        child: Icon(Icons.book),
+      ),
       body: Container(
         color: Colors.white,
-        child: QuranList(quranList: quranList),
+        child: QuranList(
+            quranList: quranList, itemScrollController: _itemScrollController),
       ),
     );
   }
