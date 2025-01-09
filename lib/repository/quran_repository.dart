66433d235f655d20fb61b865a6c5804db6976e02
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-09 19:10:44 +0700

    close db after query

--- lib/repository/quran_repository.dart
+++ lib/repository/quran_repository.dart
@@ -1,24 +1,27 @@
-import 'package:flutter/foundation.dart';
 import 'package:quran/helpers/db_helper.dart';
 import 'package:quran/models/quran_data.dart';
 
 class QuranRepository {
   static Future<List<QuranData>> getQuranBySura(int sura) async {
-    debugPrint("getQuranBySura $sura");
-
     final db = await DatabaseHelper.initializeDB();
-    final List<Map<String, dynamic>> result = await db.rawQuery(
-      '''
+    try {
+      final List<Map<String, dynamic>> result = await db.rawQuery(
+        '''
       SELECT t.*, tr.text as trText
       FROM quran_text t
       JOIN id_translation tr ON t.sura = tr.sura AND t.aya = tr.aya
       WHERE t.sura = ?
       ''',
-      [sura],
-    );
+        [sura],
+      );
 
-    List<QuranData> quran = result.map((e) => QuranData.fromJson(e)).toList();
+      List<QuranData> quran = result.map((e) => QuranData.fromJson(e)).toList();
 
-    return quran;
+      return quran;
+    } catch (e) {
+      throw Exception("Failed to load data $e");
+    } finally {
+      await db.close();
+    }
   }
 }
