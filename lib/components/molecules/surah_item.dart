eb8143cb61b33df981ea0a01fc4662ff3fb1fafa
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-09 15:07:34 +0700

    clean the component

--- lib/components/molecules/surah_item.dart
+++ lib/components/molecules/surah_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quran/components/atoms/surah/surah_atom.dart';
 import 'package:quran/models/surah_data.dart';
+import 'package:quran/router/router.dart';
 
 class SurahItem extends StatelessWidget {
   final SurahData surah;
@@ -8,40 +9,44 @@ class SurahItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 16, left: 16, right: 16),
-      padding: EdgeInsets.only(top: 16, bottom: 16, left: 16, right: 16),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withAlpha(25),
-              offset: Offset(0, 4),
-              blurRadius: 6,
-              spreadRadius: -1,
-            )
-          ]),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Left
-          _LeftComponent(surah: surah),
-          // Arabic surah name right end
-          SurahNameArabic(name: surah.name)
-        ],
-      ),
-    );
+    return InkWell(
+        onTap: () {
+          // Navigate to read screen
+          ReadScreenRoute(sura: surah.index).push(context);
+        },
+        child: Container(
+          margin: EdgeInsets.only(bottom: 16, left: 16, right: 16),
+          padding: EdgeInsets.only(top: 16, bottom: 16, left: 16, right: 16),
+          decoration: _containerDecoration(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Surah info
+              _surahDetail(),
+              // Arabic surah name right end
+              SurahNameArabic(name: surah.name)
+            ],
+          ),
+        ));
   }
-}
 
-class _LeftComponent extends StatelessWidget {
-  final SurahData surah;
+  // Container decoration
+  BoxDecoration _containerDecoration() {
+    return BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(25),
+            offset: Offset(0, 4),
+            blurRadius: 6,
+            spreadRadius: -1,
+          )
+        ]);
+  }
 
-  const _LeftComponent({required this.surah});
-
-  @override
-  Widget build(BuildContext context) {
+  // Surah detail component
+  Row _surahDetail() {
     return Row(
       spacing: 16,
       children: [
