e82582b3c97abcfcd001b06132c246e97201d2a7
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-09 19:25:55 +0700

    uses databse singleton instance

--- lib/repository/quran_repository.dart
+++ lib/repository/quran_repository.dart
@@ -2,26 +2,22 @@ import 'package:quran/helpers/db_helper.dart';
 import 'package:quran/models/quran_data.dart';
 
 class QuranRepository {
+  final dbHelper = DatabaseHelper();
+
   static Future<List<QuranData>> getQuranBySura(int sura) async {
-    final db = await DatabaseHelper.initializeDB();
-    try {
-      final List<Map<String, dynamic>> result = await db.rawQuery(
-        '''
+    final db = await DatabaseHelper().database;
+    final List<Map<String, dynamic>> result = await db.rawQuery(
+      '''
       SELECT t.*, tr.text as trText
       FROM quran_text t
       JOIN id_translation tr ON t.sura = tr.sura AND t.aya = tr.aya
       WHERE t.sura = ?
       ''',
-        [sura],
-      );
+      [sura],
+    );
 
-      List<QuranData> quran = result.map((e) => QuranData.fromJson(e)).toList();
+    List<QuranData> quran = result.map((e) => QuranData.fromJson(e)).toList();
 
-      return quran;
-    } catch (e) {
-      throw Exception("Failed to load data $e");
-    } finally {
-      await db.close();
-    }
+    return quran;
   }
 }
