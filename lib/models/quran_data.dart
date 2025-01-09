5bb1f645eafd25451da9d90e6b3c26c4ee920876
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-09 15:25:40 +0700

    add quran data model

---
+++ lib/models/quran_data.dart
@@ -0,0 +1,15 @@
+class QuranData {
+  final int index;
+  final int sura;
+  final int aya;
+  final String text;
+  final String trText;
+
+  QuranData({
+    required this.index,
+    required this.sura,
+    required this.aya,
+    required this.text,
+    required this.trText,
+  });
+}
