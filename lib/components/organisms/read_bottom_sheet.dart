068b02f79760f5c2198b65864c6066d2e6e5cb52
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-10 13:58:20 +0700

    uses default icons class because the previus third party icons make my animation janky

--- lib/components/organisms/read_bottom_sheet.dart
+++ lib/components/organisms/read_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
 import 'package:quran/components/molecules/menu_sheet/menu_item.dart';
 import 'package:quran/components/molecules/menu_sheet/menu_sheet.dart';
 
@@ -7,29 +6,29 @@ void showReadBottomSheet(BuildContext context) {
   showMenuSheet(context: context, items: [
     MenuSheetItem(
       title: "Play Murratal",
-      icon: Symbols.motion_play_rounded,
+      icon: Icons.play_circle_outline_rounded,
       onTap: () {},
     ),
     MenuSheetItem(
       title: "Copy Aya",
-      icon: Symbols.content_copy,
+      icon: Icons.content_copy,
       onTap: () {},
     ),
     MenuSheetItem(
       title: "Share Aya",
-      icon: Symbols.share,
+      icon: Icons.share,
       onTap: () {},
     ),
     MenuSheetItem(
       title: "Add To Bookmark",
-      icon: Symbols.bookmark_add_rounded,
+      icon: Icons.bookmark_add_rounded,
       filledIcon: 1,
       onTap: () {},
     ),
     MenuSheetItem(
       title: "Mark As Last Read",
       iconColor: Colors.green.shade900,
-      icon: Symbols.check,
+      icon: Icons.check,
       onTap: () {},
     ),
   ]);
