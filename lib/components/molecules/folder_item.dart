2c087904878af68a294c3fa8fd1ab4a175137245
Umar Schweinsteiger <umar.dev500@gmail.com>
2025-01-10 21:01:40 +0700

    add ripple

--- lib/components/molecules/folder_item.dart
+++ lib/components/molecules/folder_item.dart
@@ -8,42 +8,48 @@ class FolderItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ListTile(
-          iconColor: Colors.grey.shade700,
-          textColor: Colors.grey.shade700,
-          onTap: () {
-            debugPrint("navigate");
-          },
-          leading: Icon(Icons.folder_rounded),
-          trailing: InkWell(
+    return Material(
+      color: Colors.transparent,
+      child: Stack(
+        children: [
+          ListTile(
+            iconColor: Colors.grey.shade700,
+            textColor: Colors.grey.shade700,
             onTap: () {
-              debugPrint("clicked");
+              debugPrint("navigate");
             },
-            child: Icon(
-              Icons.more_horiz_rounded,
-              size: 18,
+            leading: Icon(Icons.folder_rounded),
+            trailing: InkWell(
+              splashColor: Colors.transparent,
+              onTap: () {
+                debugPrint("clicked");
+              },
+              child: Icon(
+                Icons.more_horiz_rounded,
+                size: 18,
+              ),
+            ),
+            title: Text(
+              folder.name,
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: "rcondensed",
+              ),
             ),
           ),
-          title: Text(
-            folder.name,
-            style: TextStyle(
-              fontSize: 16,
-              fontFamily: "rcondensed",
-            ),
-          ),
-        ),
-        Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Divider(
-              height: 0.5,
-              thickness: 0.5,
-              color: Colors.grey.shade300,
-            ))
-      ],
+
+          // The divier
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Divider(
+                height: 0.5,
+                thickness: 0.5,
+                color: Colors.grey.shade300,
+              ))
+        ],
+      ),
     );
   }
 }
